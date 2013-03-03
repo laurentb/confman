57c174a3af13880e2b475c4fd977468d4f46f3d6
Laurent Bachelier <laurent@bachelier.name>
2013-03-03 19:24:48 +0100

    Packaging

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='confman',
+    version='0.0',
+    description='Lazy, rootless, yet powerful config file management mostly using symlinks',
+    long_description=open('README').read(),
+    author='Laurent Bachelier',
+    author_email='laurent@bachelier.name',
+    url='http://git.p.engu.in/laurentb/confman/',
+    py_modules=['confman'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+    ],
+)

4eb4ad18cd6692c17d29e1b35ba4ea18258a5e51
Laurent Bachelier <laurent@bachelier.name>
2013-03-04 01:24:44 +0100

    Release 0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='confman',
-    version='0.0',
+    version='0.1',
     description='Lazy, rootless, yet powerful config file management mostly using symlinks',
     long_description=open('README').read(),
     author='Laurent Bachelier',

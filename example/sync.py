7dde878e67e31d4b88ac8e0bc3c95ad6de7fd551
Laurent Bachelier <laurent@bachelier.name>
2013-03-19 00:43:13 +0100

    Move samples to a single directory

--- example/sync.py
+++ example/sync.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+import os
+import sys
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, parentdir)
+
 from confman import ConfigSource
 
 options = {
@@ -6,8 +11,7 @@ options = {
     'hostname': 'test',
 }
 
-from os import path
-samples_path = path.join(path.dirname(__file__), 'samples')
+samples_path = os.path.join(os.path.dirname(__file__), 'src')
 
 c = ConfigSource(samples_path, "/tmp/dotfiles-test", None, options)
 c.sync()

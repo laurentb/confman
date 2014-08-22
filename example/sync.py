93711e7ef41b9870550713df9b4e3704f57d5815
Laurent Bachelier <laurent@bachelier.name>
2014-08-22 20:28:43 +0200

    Modernize the code for 2.6+
    Might even work with Python 3, but not tested

--- example/sync.py
+++ example/sync.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+from __future__ import absolute_import, print_function
+
 import os
 import sys
+
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, parentdir)
-
 from confman import ConfigSource
+
 
 options = {
     'tags': ['desktop'],
@@ -16,5 +19,5 @@ samples_path = os.path.join(os.path.dirname(__file__), 'src')
 c = ConfigSource(samples_path, "/tmp/dotfiles-test", None, options)
 c.sync()
 
-print
-print repr(c)
+print()
+print(repr(c))
